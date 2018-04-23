668176cf2730309d91c73bc469501274b5402964
dreamYHD <478214853@qq.com>
2018-04-23 22:09:01 +0800

    finish

--- Observer/src/cn/edu/nuc2/Client.java
+++ Observer/src/cn/edu/nuc2/Client.java
@@ -1,5 +1,7 @@
 package cn.edu.nuc2;
 
+import java.util.concurrent.*;
+
 /**
  * Created by Administrator on 2018/4/23.
  */
@@ -8,19 +10,40 @@ public class Client {
        Observable observable = new ObservableImp();
        observable.attach(new ObserverImp1());
        observable.attach(new ObserverImp2());
-       observable.attach(new Observer() {
-           @Override
-           public void next(String s) {
-               System.out.println("this is nononono");
-           }
+       FutureTask<String> future = new FutureTask<String>(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                observable.attach(new Observer() {
+                    @Override
+                    public void next(String s) {
+                        System.out.println("this is nononono");
+                    }
 
-           @Override
-           public void complete() {
-               System.out.println("this is nonono complete");
-           }
-       });
-       observable.notifyObserver("hi baby i am your father");
+                    @Override
+                    public void complete() {
+                        System.out.println("this is nonono complete");
+                    }
+                });
+                return "hello";
+            }
+        });
+        ExecutorService  executorService = new ThreadPoolExecutor(
+                5,
+                10,
+                10,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>());
+        executorService.execute(() -> observable.attach(new ObserverImp1()));
+        try {
+            if (future.get().equals("hello")){
+                observable.notifyObserver("hi baby i am your father");
+            }
 
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
 
 
 
